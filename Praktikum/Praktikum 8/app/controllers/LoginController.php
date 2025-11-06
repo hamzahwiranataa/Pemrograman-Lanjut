9c3d1c758a8b5e2f930b0287e09cf01496ecf5d1
Winata <71538946+hamzahwiranataa@users.noreply.github.com>
2025-11-06 16:16:39 +0800

    Tambah Fitur Login dan cek role pengguna

---
+++ Praktikum/Praktikum 8/app/controllers/LoginController.php
@@ -0,0 +1,57 @@
+<?php
+require_once '../app/models/LoginModel.php';
+
+class LoginController {
+    private $LoginModel;
+
+    public function __construct() {
+        $this->LoginModel = new Login();
+    }
+
+    public function index() {
+        $flash = null;
+        if (isset($_SESSION['flash'])) {
+            $flash = $_SESSION['flash'];
+            unset($_SESSION['flash']);
+        }
+        require_once '../app/views/form/login.php';
+    }
+
+    public function CheckLogin() {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $user = $this->LoginModel->CheckUser($username, $password);
+
+        if ($user) {
+            // ambil kategori user
+            $kategori = $this->LoginModel->CheckKategoriUser($username);
+            // Ambil nilai kategori dari array
+            $KategoriUser = $kategori['kategori'];
+
+            // set session berdasarkan kategori
+            $_SESSION['username'] = $username;
+
+            if ($KategoriUser == 0) {
+                $_SESSION['role'] = 'superadmin';
+                header('Location: index.php');
+                exit;
+            } elseif ($KategoriUser == 1) {
+                $_SESSION['role'] = 'operator';
+                header('Location: index.php');
+                exit;
+            } elseif ($KategoriUser == 2) {
+                $_SESSION['role'] = 'pegawai';
+                header('Location: index.php]');
+                exit;
+            }
+        } else {
+            $_SESSION['flash'] = [
+                'message' => 'Username atau password salah!',
+                'type' => 'error'
+            ];
+            header('Location: index.php');
+            exit;
+        }
+    }
+}
