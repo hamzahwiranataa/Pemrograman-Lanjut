f022a40b06b8098f18a831a7b99a2856d3e8888c
Winata <71538946+hamzahwiranataa@users.noreply.github.com>
2025-11-06 16:55:01 +0800

    Perbaiki tampilan dashboard operator

---
+++ Praktikum/Praktikum 8/app/views/operator/home.php
@@ -0,0 +1,41 @@
+<?php
+$username = $_SESSION['username'];
+$kategori = $_SESSION['role'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <br><br><br><br><br><br>
+  <center>
+    <table border="1" cellpadding="20" cellspacing="0">
+      <tr>
+        <td>
+          <center>
+            <h2>Home Dashboard</h2>
+              <table cellpadding="10">
+                <tr>
+                  <td>Username:</td>
+                  <td><?php echo htmlspecialchars($username); ?></td>
+                </tr>
+                <tr>
+                  <td>Kategori:</td>
+                  <td><?php echo htmlspecialchars($kategori); ?></td>
+                </tr>
+                <tr>
+                    <td colspan="2" align="center">
+                        <a href="index.php?action=logout">Logout</a>
+                    </td>
+                </tr>
+              </table>
+          </center>
+        </td>
+      </tr>
+    </table>
+  </center>
+</body>
+</html>
