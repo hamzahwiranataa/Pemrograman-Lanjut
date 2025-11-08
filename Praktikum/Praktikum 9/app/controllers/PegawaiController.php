ada85b5a7b58cdb143458fbe049a1c982345ea48
Winata <71538946+hamzahwiranataa@users.noreply.github.com>
2025-11-08 13:27:49 +0800

    Tambah fitur menampilkan semua data produksi, tambah produksi, edit produksi

--- Praktikum/Praktikum 9/app/controllers/PegawaiController.php
+++ Praktikum/Praktikum 9/app/controllers/PegawaiController.php
@@ -13,5 +13,39 @@ class PegawaiController {
 
         require_once '../app/views/pegawai/home.php';
     }
+
+    public function data_produksi() {
+        $datas = $this->Pegawai->GetAllData();
+        require_once '../app/views/pegawai/data_produksi.php';
+    }
+
+    public function tambah_produksi() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $tanggal = $_POST['tanggal'];
+            $varian = $_POST['varian'];
+            $jumlah_lembar = $_POST['jumlah_lembar'];
+
+            $this->Pegawai->tambah_data_produksi($tanggal, $varian, $jumlah_lembar);
+            header('Location: index.php?action=data');
+            exit;
+        }
+        require '../app/views/form/pegawai/tambahdata.php';
+    }
+
+    public function edit_produksi($id) {
+        if ($_SERVER['REQUEST_METHOD'] === "POST") {
+            $id = $_POST['id'];
+            $tanggal = $_POST['tanggal'];
+            $varian = $_POST['varian'];
+            $jumlah_lembar = $_POST['jumlah_lembar'];
+
+            $this->Pegawai->edit_data_produksi($tanggal, $varian, $jumlah_lembar, $id);
+            header('Location: index.php?action=data');
+            exit;
+        }
+
+        $data = $this->Pegawai->GetDataById($id);
+        require '../app/views/form/pegawai/editdata.php';     
+    }
 }
 ?>
